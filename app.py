c718a6a6843020454e0c842b0218b0defff366ce
Priscr23 <147168727+Priscr23@users.noreply.github.com>
2024-02-20 08:17:53 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Esto es un título')
+st.header('Esto es un headerr')
+st.markdown('*italic*')
+
+df=pd.read_csv('train.csv')
+st.dataframe(df)
